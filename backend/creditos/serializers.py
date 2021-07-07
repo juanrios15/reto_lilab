03339a9e63f2c092fb8e9370dcc0b64d9ca4f574
Juan Rios <juankrios15@gmail.com>
2021-07-07 18:49:17 -0500

    riesgo sentinel choice

--- backend/creditos/serializers.py
+++ backend/creditos/serializers.py
@@ -1,14 +1,15 @@
 from rest_framework import serializers
-
 from .models import ClienteCredito, SolicitudCredito
 
 
 class ClienteCreditoSerializer(serializers.ModelSerializer):
     
+    riesgo_sentinel = serializers.CharField(source='get_riesgo_sentinel_display')
     class Meta:
         
         model = ClienteCredito
         fields = '__all__'
+        
 
 class SolicitudCreditoSerializer(serializers.ModelSerializer):
     
