65fa9a944c0f8d9bfe1d2df2cff408439404e8b2
Juan Rios <juankrios15@gmail.com>
2021-07-07 18:37:53 -0500

    rest urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -28,4 +28,7 @@ urlpatterns = [
     # JWT auth
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Rest Framework Login urls
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
